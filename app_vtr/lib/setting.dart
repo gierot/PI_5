7d7f0a5b4586588f50634b0edf82e6bf3d4a7c2a
Gustavo da Silva Freitas <gustavo-silvafreitas@hotmail.com>
2023-06-24 07:09:24 -0300

    link aws

--- app_vtr/lib/setting.dart
+++ app_vtr/lib/setting.dart
@@ -7,7 +7,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 DataUser user_vtr = DataUser();
 
 class Settings {
-  String url = 'http://192.168.15.58:8000/api';
+  String url = 'http://18.228.214.223/api';
   Map<String, String> headers = {'Content-Type': 'application/json'};
   String _imagePath = '';
 
